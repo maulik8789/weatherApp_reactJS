5b609bb14f3a65b0446b50241e5da98eb2c5ba9c
Maulik Vaghela <maulik.vagh@gmail.com>
2024-11-19 09:04:34 -0500

    removed getOptions from gitignore for it to be accessed for deploying site.

---
+++ src/getOptions.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "Key": "895284fb2d2c50a520ea537456963d9c",
+      "Host": "https://api.openweathermap.org",
+    },
+  };
+  export const GEO_API_URL = "https://api.openweathermap.org/geo/1.0/direct";
+  
+  export const API_URL = "https://api.openweathermap.org/data/2.5";
+  export const API_KEY = "895284fb2d2c50a520ea537456963d9c";
